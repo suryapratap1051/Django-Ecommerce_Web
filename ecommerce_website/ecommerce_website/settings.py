664695aa00845a9e3756133c80a9eee1aa4c20a7
surya <suryapratap1051@gmail.com>
2024-02-23 19:47:20 +0530

    secound

--- ecommerce_website/ecommerce_website/settings.py
+++ ecommerce_website/ecommerce_website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -115,9 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = "/image/download/"
+MEDIA_ROOT = BASE_DIR
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
